dc1a4fbb2fa36b5a64178b169452821038c517a1
Mokichan <108360736+Mokichan@users.noreply.github.com>
2022-07-02 03:16:38 +0300

    Add files via upload

---
+++ 28.06 дз часть 2.py
@@ -0,0 +1,26 @@
+hg = int(input())
+
+if hg == 1:
+    print("31")
+elif hg == 3:
+    print("31")
+elif hg == 4:
+    print("30")
+elif hg == 5:
+    print("31")
+elif hg == 6:
+    print("30")
+elif hg == 7:
+    print("31")
+elif hg == 8:
+    print("31")
+elif hg == 9:
+    print("30")
+elif hg == 10:
+    print("31")
+elif hg == 11:
+    print("30")
+elif hg == 12:
+    print("31")
+elif hg == 2:
+    print("ФЕВРАЛЬ")
