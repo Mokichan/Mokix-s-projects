76dd55c9ebca41f00086eff622aac1a9cde868b0
Mokichan <108360736+Mokichan@users.noreply.github.com>
2022-07-26 20:40:27 +0300

    Add files via upload

---
+++ Hs.py
@@ -0,0 +1,9 @@
+
+hlk = [11, 43, 12]
+
+hlk2 = ["Burger", "Fry chiken", "Cola"]
+
+hd = dict(zip(hlk,hlk2 ))
+
+print(hd)
+
---
+++ hslite.py
@@ -0,0 +1,7 @@
+hlk = [11, 43, 12]
+
+hlk2 = ["Burger", "Fry chiken", "Cola"]
+
+hd = {11:"Burger", 43:"Fry chiken", 12:"Cola"}
+
+print(hd)
